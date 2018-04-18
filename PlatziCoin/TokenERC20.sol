9fb58f8ec544306aac064b061012b89a28745dac
Fernando Hernandez <ffhf.1820@gmail.com>
2018-04-18 17:43:57 -0300

    Update TokenERC20.sol

--- PlatziCoin/TokenERC20.sol
+++ PlatziCoin/TokenERC20.sol
@@ -6,15 +6,32 @@ contract TokenERC20 {
   uint8 public decimal = 18;
   uint256 public totalSupply;
   mapping (address => uint256) public balanceOf;
-  
+
   event Transfer(address from, address to, uint256 value);
 
   function TokenERC20(uint256 initialSupply, string tokenName, string tokenSymbol) public {
     totalSupply = initialSupply * 10 ** uint256(decimal);
     name = tokenName;
     symbol = tokenSymbol;
-    balanceOf[msg.sender] = totalSupply;    
-  } 
+    balanceOf[msg.sender] = totalSupply;
+  }
+
+  function _transfer(address _from, address _to, uint _value) internal {
+    require(balanceOf[_from] >= _value);
+    require(balanceOf[_to] + _value > balanceOf[_to]);
+    uint previousBalances = balanceOf[_from] + balanceOf[_to];
+
+    balanceOf[_from] -=_value;
+    balanceOf[_to] +=_value;
+    Transfer(_from,_to,_value);
+
+    assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+  }
+
+  function transfer(address _to, uint256 _value) public {
+    _transfer(msg.sende, _to, _value);
+  }
+
 }
 
 interface TokenERC20interface {
