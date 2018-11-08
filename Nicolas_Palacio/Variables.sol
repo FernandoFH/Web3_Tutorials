6d6059a662bdf8f1b9965704f52c53928c44600d
Fernando Hernandez <ffhf.1820@gmail.com>
2018-11-07 22:23:50 -0300

    Create Variables.sol

---
+++ Nicolas_Palacio/Variables.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.25;
+
+contract Variables {
+
+    string public one = 'one';
+    string two = 'two';
+    int public three = -3;
+    uint public four = 4;
+    address myAddress = 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c;
+    
+}
