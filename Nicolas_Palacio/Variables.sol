b0176becf85aba25175f5637be0fc09d9e53ddd7
FernandoFH <ffhf.1820@gmail.com>
2019-02-11 10:14:51 -0300

    Update Valiables

--- Nicolas_Palacio/Variables.sol
+++ Nicolas_Palacio/Variables.sol
@@ -6,6 +6,5 @@ contract Variables {
     string two = 'two';
     int public three = -3;
     uint public four = 4;
-    address myAddress = 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c;
-    
+    address myAddress = 0x1;   
 }
